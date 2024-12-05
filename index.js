61e5a147e674b4a73152272207d7cc7d1c360e1a
GuiNeg <GuiNeg.git@outlook.com>
2024-12-05 10:12:03 -0300

    upando a ultima att

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ vai aparecer meu nome, quando aparecer meu nome ele vai sumindo junto com o bg(b
 
 */
 
-let tl = gsap.timeline();
 const paragrafo = document.getElementById('g_sap');
 const div = document.querySelector('.overlay');
-const img = document.querySelector('.divMainFoto')
-const divMain = document.querySelector('.divMainH1eFoto')
-const sectionFoto = document.querySelectorAll('.divSectionFotos')
-const sectionfotoFirst = document.getElementById('sectionFirst')
-const sectionfotoTwo = document.getElementById('sectionTwo')
+const img = document.querySelector('.divMainFoto');
+const divMain = document.querySelector('.divMainH1eFoto');
+const sectionFoto = document.querySelectorAll('.divSectionFotos');
+const sectionfotoFirst = document.getElementById('sectionFirst');
+const sectionfotoTwo = document.getElementById('sectionTwo');
+const sectionfotoThre = document.getElementById('sectionThre');
+const sectionfotoFour = document.getElementById('sectionFour');
+const sectionfotofive = document.getElementById('sectionFive');
 
 //o primeiro parametro do gsap é o elemento que quermos animar
 //o segundo parametro são as propriedades do css que quero definir o valor
@@ -31,46 +33,88 @@ gsap.to(paragrafo, {
   duration: 1.5,
 }); */
 
+let tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.divSectionFotos',
+    start: '10% center',
+  },
+});
+tl2
+  .from(
+    sectionfotoThre,
+    {
+      rotateZ: 50,
+      duration: 1.2,
+      ease: 'power4.out',
+      scale: 0,
+    },
+    '<',
+  )
+  .from(
+    sectionfotoFour,
+    {
+      rotateZ: 55,
+      duration: 1.4,
+      ease: 'power4.out',
+      scale: 0,
+    },
+    '<',
+  )
+  .from(
+    sectionfotofive,
+    {
+      rotateZ: 55,
+      duration: 1.4,
+      ease: 'power4.out',
+      scale: 0,
+    },
+    '<',
+  );
 
-
-
-
-
-
-
-tl.set("body",{overflow:"hidden"}).
-from(paragrafo, {
-  opacity: 0,
-  duration: 1,
-}).
-to(paragrafo, 
-  { opacity: 0, 
-    delay: 1.1, 
-    duration: 0.4 })
-    .to(div,{
-      opacity: 0,
-      duration:1,
-      ease: "linear"
-}).from(img,{
-  rotateZ: 45,
-  duration: 1.5,
-  ease: "power4.inOut",
-  scale: 0,
-}, "<")
-.from(sectionfotoFirst,{
-  rotateZ: 50,
-  duration: 1.2,
-  ease: "power4.out",
-  scale: 0,
-},"<")
-.from(sectionfotoTwo,{
-  rotateZ: 55,
-  duration: 1.4,
-  ease: "power4.out",
-  scale: 0,
-},"<").set("body",{overflow:"auto"}).set(div,{
-  zIndex: "-1"
-})
-
-
-  
+//
+let tl = gsap.timeline();
+tl.set('body', { overflow: 'hidden' })
+  .from(paragrafo, {
+    opacity: 0,
+    duration: 1,
+  })
+  .to(paragrafo, { opacity: 0, delay: 1.1, duration: 0.4 })
+  .to(div, {
+    opacity: 0,
+    duration: 1,
+    ease: 'linear',
+  })
+  .set(div, {
+    zIndex: '-1',
+  })
+  .set('body', { overflow: 'auto' })
+  .from(
+    img,
+    {
+      rotateZ: 45,
+      duration: 1.5,
+      ease: 'power4.inOut',
+      scale: 0,
+    },
+    '<',
+  )
+  .from(
+    sectionfotoFirst,
+    {
+      rotateZ: 50,
+      duration: 1.2,
+      ease: 'power4.out',
+      scale: 0,
+    },
+    '<',
+  )
+  .from(
+    sectionfotoTwo,
+    {
+      rotateZ: 50,
+      duration: 1.2,
+      ease: 'power4.out',
+      scale: 0,
+    },
+    '<',
+  );
